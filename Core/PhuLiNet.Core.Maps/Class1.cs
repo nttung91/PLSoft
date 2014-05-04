6ca2c85b0f612cd6a2c4fe5eff3854ecec785603
nttung91 <plsoft2014@gmail.com>
2014-05-04 15:33:44 +0700

    Initialize project

---
+++ Core/PhuLiNet.Core.Maps/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhuLiNet.Core.Maps
+{
+    public class Class1
+    {
+    }
+}
