8444dbf84c445399b2026ad2b70a7059227fd741
nttung91 <plsoft2014@gmail.com>
2014-05-10 21:20:10 +0700

    Host Application

---
+++ Main/PhuLiNet.Window.MainApplication/FrmStartForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Core.BaseForms;
+
+namespace PhuLiNet.Window.MainApplication
+{
+    public partial class Form1 : FrmBase
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
